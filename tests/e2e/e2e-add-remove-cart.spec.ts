d147bfcb0413d9123604994ac1069ca5cd99f70b
Markus Frutuoso <markusviniciosfr@gmail.com>
2026-09-19 11:49:45 -0300

    update: e2e add and remove cart

--- tests/e2e/e2e-add-remove-cart.spec.ts
+++ tests/e2e/e2e-add-remove-cart.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@Playwright/test'
 
-test.describe.parallel('Login / Logout Flow', () => {
+test.describe.parallel('Add and Remove Items', () => {
   // Before Hook
   test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/')
+    await page.fill('#user-name', 'standard_user')
+    await page.fill('#password', 'secret_sauce')
+    await page.click('#login-button')
+  })
+
+  test("Add Item to the Cart After Login Page", async ({page}) => {
+    await page.click("#addZ-to-cart-sauce-labs-backpack")
   })
